5251f643f73f22307858dac1acddc88bc90e9800
runehand <101655477+runehand@users.noreply.github.com>
2026-06-07 11:53:01 -0600

    fix build config (#81)

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -4,17 +4,20 @@ import { ENCRYPTION_KEY } from 'src/config-global';
 
 const IV_LENGTH = 16; // For AES, this is always 16
 
-if (!ENCRYPTION_KEY || ENCRYPTION_KEY.length !== 64) {
-    throw new Error('Invalid encryption key length. It must be 64 hexadecimal characters.');
-}
+function getKey() {
+    if (!ENCRYPTION_KEY || ENCRYPTION_KEY.length !== 64) {
+        throw new Error('Invalid encryption key length. It must be 64 hexadecimal characters.');
+    }
 
-const key = Buffer.from(ENCRYPTION_KEY, 'hex') as unknown as crypto.CipherKey;
+    return Buffer.from(ENCRYPTION_KEY, 'hex') as unknown as crypto.CipherKey;
+}
 
 export function encrypt(text: string): string {
     const iv = crypto.randomBytes(IV_LENGTH);
+    const key = getKey();
     const cipher = crypto.createCipheriv(
         'aes-256-cbc',
-        key as unknown as crypto.CipherKey,
+        key,
         iv as unknown as crypto.BinaryLike,
     );
     const encryptedChunk: any = cipher.update(text);
@@ -30,9 +33,10 @@ export function decrypt(text: string): string {
     const textParts = text.split(':');
     const iv = Buffer.from(textParts.shift()!, 'hex');
     const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const key = getKey();
     const decipher = crypto.createDecipheriv(
         'aes-256-cbc',
-        key as unknown as crypto.CipherKey,
+        key,
         iv as unknown as crypto.BinaryLike,
     );
     const decryptedChunk: any = decipher.update(encryptedText as any);
